f904fc2bdaace6739fdbad1f9f0431db1092fa4c
nkneeraj <nkneerj641@gmail.com>
2020-11-06 23:50:50 +0530

    [NK]Added: EmpWage calculation for max 20 days and max 100hrs.

--- EmpWage.js
+++ EmpWage.js
@@ -1,16 +1,6 @@
 
 //UC-1
-
 const IS_ABSENT=0;
-
-// let empCheck = Math.floor(Math.random()*10)%2;
-// if(empCheck==IS_ABSENT){
-//     console.log("UC1-Employee is ABSENT,Exiting the program");
-//     return;
-// } else{
-//     console.log("UC1-Employee is PRESENT.");
-// }
-
 //UC-2 refactored to UC3
 
 const IS_PART_TIME=1;
@@ -19,6 +9,9 @@ const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
 const NUM_OF_WORKING_DAYS = 20;
+const MAX_HRS_IN_MONTH = 100;
+let totalWorkingDays = 0;
+let totalEmpHrs = 0;
 
 function getWorkingHours(empCheck){
 switch(empCheck){
@@ -32,11 +25,22 @@ switch(empCheck){
 }
 
 //UC4- Emp wage for 20 days
-let empHrs = 0;
-for(let day=0; day<NUM_OF_WORKING_DAYS;day++){
-    let empCheck  = Math.floor(Math.random()*10)%3;
-    empHrs+= getWorkingHours(empCheck);
+
+// for(let day=0; day<NUM_OF_WORKING_DAYS;day++){
+//     let empCheck  = Math.floor(Math.random()*10)%3;
+//     empHrs+= getWorkingHours(empCheck);
+// }
+
+// let empWage = empHrs * WAGE_PER_HOUR;
+// console.log("UC4 - Emp Wage for 20 days:"+ empWage);
+
+//UC5
+
+while(totalEmpHrs<=MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS){
+    totalWorkingDays++;
+    let empCheck = Math.floor(Math.random()*10)%3;
+    totalEmpHrs+= getWorkingHours(empCheck);
 }
 
-let empWage = empHrs * WAGE_PER_HOUR;
-console.log("UC4 - Emp Wage for 20 days:"+ empWage);
+let empWage = totalEmpHrs * WAGE_PER_HOUR;
+console.log("UC5 - Emp Wage for 20 days:"+ empWage);
