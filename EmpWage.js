5399167f5c578991ed395177798bbf054492232e
nkneeraj <nkneerj641@gmail.com>
2020-11-06 23:32:38 +0530

    [Nk]Added: EmpWage calculation for 20 days.

--- EmpWage.js
+++ EmpWage.js
@@ -18,6 +18,7 @@ const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
+const NUM_OF_WORKING_DAYS = 20;
 
 function getWorkingHours(empCheck){
 switch(empCheck){
@@ -30,7 +31,12 @@ switch(empCheck){
 }
 }
 
-let empCheck = Math.floor(Math.random()*10)%2;
-let empHrs = getWorkingHours(empCheck);
+//UC4- Emp wage for 20 days
+let empHrs = 0;
+for(let day=0; day<NUM_OF_WORKING_DAYS;day++){
+    let empCheck  = Math.floor(Math.random()*10)%3;
+    empHrs+= getWorkingHours(empCheck);
+}
+
 let empWage = empHrs * WAGE_PER_HOUR;
-console.log("UC2 - Emp Wage:"+ empWage);
+console.log("UC4 - Emp Wage for 20 days:"+ empWage);
