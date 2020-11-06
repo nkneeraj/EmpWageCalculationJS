c50126cd459decddfad24a9f046938b7e4ea5608
nkneeraj <nkneerj641@gmail.com>
2020-11-06 10:18:18 +0530

    [NK]Added: Employee attendance check.

---
+++ UC1.js
@@ -0,0 +1,13 @@
+
+//UC-1
+
+const IS_ABSENT=0;
+let empCheck = Math.floor(Math.random()*10)%2;
+if(empCheck==IS_ABSENT){
+    console.log("UC1-Employee is ABSENT,Exiting the program");
+    return;
+} else{
+    console.log("UC1-Employee is PRESENT.");
+}
+
+
