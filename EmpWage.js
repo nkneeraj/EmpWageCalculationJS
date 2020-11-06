2250ec7b3b2d9f76f85f5f3160bb87497ecc5fff
nkneeraj <nkneerj641@gmail.com>
2020-11-06 11:13:06 +0530

    [NK]Refactored: Wage calculation refactored.

--- EmpWage.js
+++ EmpWage.js
@@ -2,15 +2,16 @@
 //UC-1
 
 const IS_ABSENT=0;
-let empCheck = Math.floor(Math.random()*10)%2;
-if(empCheck==IS_ABSENT){
-    console.log("UC1-Employee is ABSENT,Exiting the program");
-    return;
-} else{
-    console.log("UC1-Employee is PRESENT.");
-}
 
-//UC-2
+// let empCheck = Math.floor(Math.random()*10)%2;
+// if(empCheck==IS_ABSENT){
+//     console.log("UC1-Employee is ABSENT,Exiting the program");
+//     return;
+// } else{
+//     console.log("UC1-Employee is PRESENT.");
+// }
+
+//UC-2 refactored to UC3
 
 const IS_PART_TIME=1;
 const IS_FULL_TIME = 2;
@@ -18,16 +19,18 @@ const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
 
-let empHrs = 0;
+function getWorkingHours(empCheck){
 switch(empCheck){
     case IS_PART_TIME:
-        empHrs = PART_TIME_HOURS;
-        break;
+        return PART_TIME_HOURS;
     case IS_FULL_TIME:
-        empHrs = FULL_TIME_HOURS;
-        break;
+       return FULL_TIME_HOURS;
     default:
-        empHrs=0;
+        return 0;
 }
+}
+
+let empCheck = Math.floor(Math.random()*10)%2;
+let empHrs = getWorkingHours(empCheck);
 let empWage = empHrs * WAGE_PER_HOUR;
 console.log("UC2 - Emp Wage:"+ empWage);
